0c545d322ad1a77d8c57cc9d7c0d395a05a3c006
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2022-01-07 12:03:47 -0300

    refactoring

--- data_lake/cleaning_data/startupbase_normalize.py
+++ data_lake/cleaning_data/startupbase_normalize.py
@@ -7,27 +7,35 @@ from dotenv import load_dotenv
 df = pd.read_parquet('./raw_data/startup.parquet')
 
 df['name'] = df['name'].str.lower().str.strip()
+
 df['cidade_estado'] = df['cidade_estado'].str.lower().str.strip()
+df['cidade_estado'] = [unidecode(one) for one in df['cidade_estado']]
+
 df['mercado'] = df['mercado'].str.lower().str.strip()
 df['mercado'] = [unidecode(mercado) for mercado in df['mercado']]
-df.loc[df['mercado'] == 's/n', 'mercado'] = None
+
 df['modelo'] = df['modelo'].str.lower().str.strip()
 df['modelo'] = [unidecode(modelo) for modelo in df['modelo']]
-df.loc[df['modelo'] == 's/n', 'modelo'] = None
+
 df['momento'] = df['momento'].str.lower().str.strip()
 df['momento'] = [unidecode(momento) for momento in df['momento']]
-df.loc[df['momento'] == 's/n', 'momento'] = None
+
 df['tamanho'] = df['tamanho'].str.lower().str.strip()
 df['tamanho'] = [unidecode(tamanho) for tamanho in df['tamanho']]
-df.loc[df['tamanho'] == 'n/d', 'tamanho'] = None
-df.loc[df['tamanho'] == '(nao informado)', 'tamanho'] = None
+
 df['modelo de receita'] = df['modelo de receita'].str.lower().str.strip()
-df['modelo de receita'] = [unidecode(modelo_receita)
-                           for modelo_receita in df['modelo de receita']]
-df.loc[df['modelo de receita'] == 's/n', 'modelo de receita'] = None
+df['modelo de receita'] = [unidecode(modelo_receita) for modelo_receita in df['modelo de receita']]
+
 df['segmento'] = df['segmento'].str.lower().str.strip()
 df['segmento'] = [unidecode(segmento) for segmento in df['segmento']]
+
+df.loc[df['mercado'] == 's/n', 'mercado'] = None
+df.loc[df['modelo'] == 's/n', 'modelo'] = None
+df.loc[df['momento'] == 's/n', 'momento'] = None
+df.loc[df['tamanho'] == 'n/d', 'tamanho'] = None
+df.loc[df['modelo de receita'] == 's/n', 'modelo de receita'] = None
 df.loc[df['segmento'] == 's/n', 'segmento'] = None
+df.loc[df['tamanho'] == '(nao informado)', 'tamanho'] = None
 
 df['momento'] = df['momento'].str.lower().str.strip()
 
@@ -37,14 +45,13 @@ df['cidade'] = ['' for row in df['name']]
 for i in range(len(df['name'])):
     ci_es = df['cidade_estado'][i]
     ci_es = str(ci_es).split(' - ')
-    # print(ci_es)
     if len(ci_es) >= 2:
         df['estado'][i] = unidecode(ci_es[1])
         df['cidade'][i] = unidecode(ci_es[0])
 
 df = df.drop(['cidade_estado', 'redes'], axis=1)
 
-load_dotenv('../../login.env')
+load_dotenv('login.env')
 host = getenv('DBHOST')
 user = getenv('DBUSER')
 passwd = getenv('DBPASS')
